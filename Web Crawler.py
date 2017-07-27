50f832693c53c72e6a15ed629d7a70578c646e4c
William Arevalo <willi11-95@hotmail.com>
2017-07-27 15:31:24 -0500

    Add counter for links

--- Web Crawler.py
+++ Web Crawler.py
@@ -33,12 +33,24 @@ def get_all_links(page):
             break
     return links
 
+def record_user_click(index, keyword, url):
+    urls = lookup(index, keyword)
+    if urls:
+        for entry in urls:
+            if entry[0] == url:
+                entry[1] = entry[1]+1
+
 def add_to_index(index, keyword, url):
+    # format of index: [[keyword, [[url, count], [url, count],..]],...]
     for entry in index:
         if entry[0] == keyword:
-            entry[1].append(url)
+            for urls in entry[1]:
+                if urls[0] == url:
+                    return
+            entry[1].append([url,0])
             return
-    index.append([keyword, [url]])
+    # not found, add new keyword to index
+    index.append([keyword, [[url,0]]])
 
 def lookup(index, keyword):
     for entry in index:
